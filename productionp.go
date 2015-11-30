5614480ab7f1570609bd74f933ffe32a76cb071c
Sushant Srivastava <sushant.srivastav@gmail.com>
2015-12-01 02:37:59 +0530

    Added Dynamic Programming Walter Whitin Model

--- productionp.go
+++ productionp.go
@@ -95,7 +95,65 @@ func SilverMeal(mps_input MPSInput) MPSOutput {
 		i = j
 	}
 	logger.Printf("%v ", plan)
-	logger.Printf("Total holding cost :%.2f, Total Order Cost : .2f", total_holding_cost, total_setup_cost)
+	logger.Printf("Total holding cost :%.2f, Total Order Cost : %.2f", total_holding_cost, total_setup_cost)
 	return MPSOutput{plan: plan, total_cost: total_holding_cost + total_setup_cost,
 		holding_cost: total_holding_cost, setup_cost: total_setup_cost}
+}
+
+func WagnerWhitin(input MPSInput) MPSOutput {
+	dynamic_table := make([][]float32, len(input.forecasts))
+	result := make([]int, len(input.forecasts))
+
+	calculate := func(forecasts []int, setup_cost float32, holding_cost float32) float32 {
+		total_cost := setup_cost
+		for i := 1; i < len(forecasts); i++ {
+			total_cost += float32(i) * float32(forecasts[i]) * holding_cost
+		}
+		return total_cost
+	}
+	minimum := func(list []float32) float32 {
+		min_val := float32(10000000)
+		for _, val := range list {
+			if val < min_val {
+				min_val = val
+			}
+		}
+		return min_val
+	}
+	minimumWithIndex := func(list []float32) (float32, int) {
+		min_val := float32(10000000)
+		index := -1
+		for i, val := range list {
+			if val < min_val {
+				min_val = val
+				index = i
+			}
+		}
+		return min_val, index
+	}
+
+	for i := 0; i < len(input.forecasts); i++ {
+		dynamic_table[i] = make([]float32, i+1)
+		dynamic_table[i][0] = calculate(input.forecasts[:i+1], input.order_cost, input.holding_cost)
+		for j := 0; j < i; j++ {
+			dynamic_table[i][j+1] = calculate(input.forecasts[j+1:i+1], input.order_cost, input.holding_cost) + minimum(dynamic_table[j])
+		}
+	}
+	order_quantity := 0
+	total_setup_amount := float32(0)
+
+	for j := len(input.forecasts) - 1; j >= 0; j-- {
+		_, i := minimumWithIndex(dynamic_table[j])
+		order_quantity += input.forecasts[j]
+		if i == j {
+			result[j] = order_quantity
+			order_quantity = 0
+			total_setup_amount += input.order_cost
+		} else {
+			result[j] = 0
+		}
+
+	}
+
+	return MPSOutput{plan: result, setup_cost: total_setup_amount}
 }
--- productionp_test.go
+++ productionp_test.go
@@ -1,6 +1,8 @@
 package productionp
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestLevel(t *testing.T) {
 	forecasts := []int{120, 130, 100, 150}
@@ -28,8 +30,8 @@ func TestChase(t *testing.T) {
 	initial := 10
 	minimum_invetory := 10
 	chase := Chase(MPSInput{forecasts: forecasts, initial_inventory: initial, minimum_inventory: minimum_invetory})
-	if 12 != chase[0] {
-		t.Fatalf("Expected %d but got %d", 12, chase[1])
+	if 0 != chase.plan[0] {
+		t.Fatalf("Expected %d but got %d", 12, chase.plan[1])
 	}
 }
 
@@ -38,6 +40,28 @@ func TestSilverMeal(t *testing.T) {
 		initial_inventory: 0, minimum_inventory: 0, holding_cost: 5.0, order_cost: 600.0}
 
 	output := SilverMeal(mps)
+	if 4 != len(output.plan) {
+		t.Fatalf(" Expected length of plan is : %d but got %d", 4, len(output.plan))
+	}
+	if 120 != output.plan[0] {
+		t.Fatalf(" Expected first output is %d but got %d", 120, output.plan[0])
+	}
+	if 230 != output.plan[1] {
+		t.Fatalf(" Expected second output is %d but got %d", 230, output.plan[1])
+	}
+	if 0 != output.plan[2] {
+		t.Fatalf(" Expected third output is %d but got %d", 0, output.plan[2])
+	}
+	if 150 != output.plan[3] {
+		t.Fatalf(" Expected fourth output is %d but got %d", 120, output.plan[3])
+	}
+
+}
+
+func TestWalterWhitin(t *testing.T) {
+	mps := MPSInput{forecasts: []int{120, 130, 100, 150},
+		initial_inventory: 0, minimum_inventory: 0, holding_cost: 5.0, order_cost: 600.0}
+	output := WagnerWhitin(mps)
 	if 4 != len(output.plan) {
 		t.Fatalf(" Expected length of plan is : %d but got %d", 4, len(output.plan))
 	}
